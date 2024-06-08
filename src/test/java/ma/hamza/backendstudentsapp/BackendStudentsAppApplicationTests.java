60bced91c92d02c8155cb3d28f5f480e7fd05943
hamzabourass <roxmax224@gmail.com>
2024-06-08 22:39:46 +0100

    fixed unit tests

--- src/test/java/ma/hamza/backendstudentsapp/BackendStudentsAppApplicationTests.java
+++ src/test/java/ma/hamza/backendstudentsapp/BackendStudentsAppApplicationTests.java
@@ -1,8 +1,13 @@
 package ma.hamza.backendstudentsapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @SpringBootTest
 class BackendStudentsAppApplicationTests {
 
